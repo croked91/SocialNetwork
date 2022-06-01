fe988b5df56e854541f32627c10d3099134dbad4
Croked1991 <atik.egor@gmail.com>
2022-06-01 22:15:33 +0300

    added refs

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -22,18 +22,18 @@ type PostTextType = {
   const MyPosts = (props: PostDataType) => {
 
 
-  const [postText, setPostText] = useState<PostTextType>({ id: 1, post: '', likes: 0 })
+  // const [postText, setPostText] = useState<PostTextType>({ id: 1, post: '', likes: 0 })
 
 
+  let newPostElement = React.createRef<HTMLInputElement>()
 
-  const onChangeHandler = (e: string) => {
-    setPostText({ id: 0, post: e, likes: 0 })
-  }
+  // const onChangeHandler = (e: string) => {
+  //   setPostText({ id: 0, post: e, likes: 0 })
+  // }
 
 
   const onClickHandler = (postText: PostTextType) => {
     props.addPost(postText)
-    setPostText({ id: 0, post: '', likes: 0 })
   }
 
   let postsData = props.state.posts.map(post => (<Post id={post.id} message={post.post} likes={post.likes} />))
@@ -43,8 +43,9 @@ type PostTextType = {
       <h1>My posts</h1>
       <h3>New post</h3>
       <div>
-        <input value={postText.post} onChange={(event) => onChangeHandler(event.currentTarget.value)}></input>
-        <button onClick={() => onClickHandler(postText)} className={s.button}>Publish post</button>
+      {/* onChange={(event) => onChangeHandler(event.currentTarget.value)} */}
+        <input ref={newPostElement} value={newPostElement.current?.value}></input>
+        <button onClick={() => onClickHandler({id: 1, post: newPostElement.current?.value || '', likes: 0})} className={s.button}>Publish post</button>
       </div>
       <div className={s.posts}>
         {postsData}
